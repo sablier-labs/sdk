d23411de063b1183952e212088307f6f5c08023d
Paul Razvan Berg <prberg@proton.me>
2025-10-02 11:38:42 -0400

    refactor: make isBroadcastsUnified clearer

--- tests/helpers/broadcasts.ts
+++ tests/helpers/broadcasts.ts
@@ -3,13 +3,15 @@ import type { Sablier } from "@src/types";
 /**
  * @notice Determines if a release uses the same broadcast directory for ZK and non-ZK chains.
  *
- * @dev For the earlier versions, deployments on ZK chains were broadcasted using Hardhat while deployments on non-ZK chains
- * used Foundry, which resulted in separate broadcast structures. In the release versions checked here we switched to
- * Foundry for every chain, so the broadcast output is now unified.
+ * Previously, deployments on ZK chains used to be made using Hardhat, while deployments on non-ZK chains used Foundry.
+ * This resulted in separate broadcast structures. In the release versions checked here we switched to Foundry for
+ * every chain, so the broadcast output is now unified.
  */
 export function isBroadcastsUnified(release: Sablier.Release): boolean {
+  const majorVersion = Number(release.version[1]);
   return (
-    ((release.protocol === "airdrops" || release.protocol === "flow") && release.version.startsWith("v2.")) ||
-    (release.protocol === "lockup" && release.version.startsWith("v3."))
+    (release.protocol === "airdrops" && majorVersion >= 2) ||
+    (release.protocol === "flow" && majorVersion >= 2) ||
+    (release.protocol === "lockup" && majorVersion >= 3)
   );
 }
