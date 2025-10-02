c95789c2c448b14c60a800083430ab996ffdf107
smol-ninja <shubhamy2015@gmail.com>
2025-10-02 12:05:21 +0100

    add explaination about unified broadcast structure

--- tests/helpers/broadcasts.ts
+++ tests/helpers/broadcasts.ts
@@ -1,7 +1,11 @@
 import type { Sablier } from "@src/types";
 
 /**
- * Determines if a release uses the same broadcast directory for ZK and non-ZK chains.
+ * @notice Determines if a release uses the same broadcast directory for ZK and non-ZK chains.
+ *
+ * @dev For the earlier versions, deployments on ZK chains were broadcasted using Hardhat while deployments on non-ZK chains
+ * used Foundry, which resulted in separate broadcast structures. In the release versions checked here we switched to
+ * Foundry for every chain, so the broadcast output is now unified.
  */
 export function isBroadcastsUnified(release: Sablier.Release): boolean {
   return (
