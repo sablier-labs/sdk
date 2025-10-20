5610408f80f1c2b2876bf2ec2343edfee97990ab
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2025-10-20 15:50:16 +0300

    feat: add solana chains

---
+++ src/solana/types.ts
@@ -0,0 +1,49 @@
+import type { Chain as ViemChain } from "viem";
+
+/**
+ * @see https://github.com/wevm/viem/discussions/3678
+ */
+type ChainBlockExplorer = {
+  name: string;
+  url: string;
+  apiUrl?: string | undefined;
+};
+
+export namespace Solana {
+  export type Address = string;
+  export type ChainCode = "SOL" | "SOLDEV" | "SOLTEST";
+  export type Cluster = "mainnet-beta" | "devnet" | "testnet";
+
+  export type Chain = ViemChain & {
+    blockExplorers: {
+      [key: string]: ChainBlockExplorer;
+      default: ChainBlockExplorer;
+    };
+    /** Whether this chain is supported by the Sablier Interface at https://app.sablier.com. */
+    isSupportedByUI: boolean;
+    /** Whether this is a testnet network. */
+    isTestnet: boolean;
+    /** Whether this is a zkEVM like zkSync. */
+    nativeCurrency: ViemChain["nativeCurrency"] & {
+      coinGeckoId: string;
+    };
+    slug: string;
+
+    rpc: {
+      /** Helius RPC URL generator. */
+      helius?: (apiKey: string) => string;
+      /** Default RPC URL. */
+      defaults: string[];
+    };
+    /** Used in deployment files to identify the chain, e.g., arbitrum-sepolia. */
+    chainlink: {
+      program: Address; // Chainlink program used to retrieve on-chain price feeds
+      feed: Address; // Account providing the SOL/USD price feed data.
+    };
+    definition: {
+      chainCode: ChainCode;
+      chainId: number;
+      cluster: Cluster;
+    };
+  };
+}
