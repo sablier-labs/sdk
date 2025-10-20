4c1e968e77e6b9ec44e364541459b0ba124b61b0
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2025-10-20 11:21:55 +0300

    feat: add solana idls

---
+++ src/solana/idl/airdrops/v1.0/index.ts
@@ -0,0 +1,2 @@
+export { default as SablierMerkleInstantV10IDL } from "./SablierMerkleInstantV10";
+export type { default as SablierMerkleInstantV10Type } from "./SablierMerkleInstantV10Type";
---
+++ src/solana/idl/index.ts
@@ -0,0 +1,2 @@
+export * from "./airdrops";
+export * from "./lockup";
