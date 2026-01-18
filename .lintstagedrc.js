f83fab524fa1fc344753e80ca09dd685c05c8650
Paul Berg <prberg@proton.me>
2026-01-18 22:24:32 +0200

    fix: convert lint-staged config to ESM

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 /**
  * @type {import("lint-staged").Configuration}
  */
-module.exports = {
+export default {
   "*.{json,jsonc,ts}": "bun biome check --write",
   "*.{md,yml}": "bun prettier --cache --write",
   "*.ts": "bun biome lint --write --only correctness/noUnusedImports",
