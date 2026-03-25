ae962d6f7ca822a01826532b82053bfddeebcd8f
smol-ninja <shubhamy2015@gmail.com>
2026-03-25 14:33:05 +0000

    fix: add missing BobVaultShare ABI

--- src/evm/releases/bob/v1.0/aliases.ts
+++ src/evm/releases/bob/v1.0/aliases.ts
@@ -1,6 +1,7 @@
 import manifest from "./manifest.js";
 
 const aliases = {
+  [manifest.BOB_VAULT_SHARE]: "BOB_VAULT_SHARE",
   [manifest.SABLIER_BOB]: "BOB",
   [manifest.SABLIER_ESCROW]: "ESCROW",
   [manifest.SABLIER_LIDO_ADAPTER]: "LIDO_ADAPTER",
--- src/evm/releases/bob/v1.0/manifest.ts
+++ src/evm/releases/bob/v1.0/manifest.ts
@@ -1,4 +1,5 @@
 const manifest = {
+  BOB_VAULT_SHARE: "BobVaultShare",
   SABLIER_BOB: "SablierBob",
   SABLIER_ESCROW: "SablierEscrow",
   SABLIER_LIDO_ADAPTER: "SablierLidoAdapter",
